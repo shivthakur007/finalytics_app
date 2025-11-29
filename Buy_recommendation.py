14a61459f7d447d0661be918562c8f875ac7a0c1
Shiv Thakur <125086310+shivthakur007@users.noreply.github.com>
2025-11-29 17:44:51 +0530

    Update Buy_recommendation.py

--- Buy_recommendation.py
+++ Buy_recommendation.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Car Loan Recommendation App")
+st.title("Buy Your Car")
 
 # User Inputs
 Mon_Income = st.number_input("Monthly Income:", min_value=0, step=100)
@@ -12,10 +12,10 @@ downpayment = st.number_input("Down Payment:", min_value=0, step=500)
 # Process Button
 if st.button("Check Recommendation"):
 
-    if downpayment > 0.2 * Car_price:
+    if downpayment >= 0.2 * Car_price:
         st.error("❌ Not Recommended: Down payment should not exceed 20% of the car price.")
 
-    elif loan_tenure > 4:
+    elif loan_tenure <= 4:
         st.error("❌ Not Recommended: Loan tenure should not exceed 4 years.")
 
     elif Expense > 0.1 * Mon_Income:
