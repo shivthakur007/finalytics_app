00f724efa181de1990046d53b0390232991872c9
Shiv Thakur <125086310+shivthakur007@users.noreply.github.com>
2025-11-29 17:53:28 +0530

    Update Buy_recommendation.py

--- Buy_recommendation.py
+++ Buy_recommendation.py
@@ -12,11 +12,11 @@ downpayment = st.number_input("Down Payment:", min_value=0, step=500)
 # Process Button
 if st.button("Check Recommendation"):
 
-    if downpayment >= 0.2 * Car_price:
-        st.error("❌ Not Recommended: Down payment should not exceed 20% of the car price.")
+    if downpayment =< 0.2 * Car_price:
+        st.success("Recommended: Down payment should be exceed 20% or more of the car price.")
 
-    elif loan_tenure <= 4:
-        st.error("❌ Not Recommended: Loan tenure should not exceed 4 years.")
+    elif loan_tenure =< 4:
+        st.success("❌ Not Recommended: Loan tenure should be 4 years or less.")
 
     elif Expense > 0.1 * Mon_Income:
         st.error("❌ Not Recommended: Expense exceeds 10% of monthly income.")
