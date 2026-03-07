4334cffc245dae79d56c678abc32c47444ad471e
Shiv Thakur <125086310+shivthakur007@users.noreply.github.com>
2026-03-07 08:42:52 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import ast
+import operator
 
 st.markdown(
     """
@@ -15,6 +17,56 @@ calculator = st.sidebar.selectbox(
     "Choose Calculator",
     ["Financial", "Simple", "Scientific"]
 )
+
+# Operators Mapping
+OPERATORS = {
+    ast.Add: operator.add,
+    ast.Sub: operator.sub,
+    ast.Mult: operator.mul,
+    ast.Div: operator.truediv,
+    ast.Mod: operator.mod,
+    ast.Pow: operator.pow
+}
+
+# Evaluation Function
+def evaluate(expression):
+
+    def _eval(node):
+
+        if isinstance(node, ast.Constant):
+            if isinstance(node.value, (int, float)):
+                return node.value
+            else:
+                raise TypeError("Only numeric values allowed")
+
+        elif isinstance(node, ast.BinOp):
+            left = _eval(node.left)
+            right = _eval(node.right)
+
+            op_type = type(node.op)
+            if op_type in OPERATORS:
+                return OPERATORS[op_type](left, right)
+            else:
+                raise TypeError("Operator not supported")
+
+        elif isinstance(node, ast.UnaryOp):
+            operand = _eval(node.operand)
+
+            if isinstance(node.op, ast.USub):
+                return -operand
+
+            elif isinstance(node.op, ast.UAdd):
+                return operand
+
+            else:
+                raise TypeError("Unary operator not supported")
+
+        else:
+            raise TypeError("Unsupported expression")
+
+    tree = ast.parse(expression, mode='eval')
+    return _eval(tree.body)
+
 
 def simple_interest(p, r, t):
     return (p * r * t) / 100
@@ -79,3 +131,20 @@ if calculator == "Financial":
         if st.button("Calculate"):
             result = compound_value(pv, r, n)
             st.success(f"Future Value = {result}")
+
+if calculator == "Simple":
+    st.title("Simple Calculator 🧮")
+    # User Input
+    expression = st.text_input("Enter Expression", "2 + 3 * 5")
+
+    # Calculate Button
+    if st.button("Calculate"):
+
+        try:
+            result = evaluate(expression)
+            st.success(f"Result: {result}")
+
+        except Exception as e:
+            st.error(f"Error: {e}")
+
+
